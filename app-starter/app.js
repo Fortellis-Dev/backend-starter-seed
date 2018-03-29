b56b98ea3c0efbad5422272e40f54eb6a3b6dc38
Naveen, Dokuparthi <naveen.dokuparthi@cdk.com>
2018-03-29 18:05:15 -0500

    adding catch-all error handler

--- app-starter/app.js
+++ app-starter/app.js
@@ -4,15 +4,15 @@
  * Load Dependencies
  */
 const express = require('express'),
-      bodyParser = require('body-parser'),
-      path = require('path');
+    bodyParser = require('body-parser'),
+    path = require('path');
 
 
-const DEFAULT_PORT = 3000;    
+const DEFAULT_PORT = 3000;
 
 /**
  * Load application routes   
- */    
+ */
 const configureAppointmentRoutes = require('./src/appointments/appointments.routes');
 
 /**
@@ -41,8 +41,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 configureAppointmentRoutes(app);
 
 /**
+ * Adding global eror handler
+ */
+app.use(function errorHandler(err, req, res, next) {
+    res.status(500)
+    res.json({ error: err.toString() });
+});
+
+/**
  * Start express server
  */
-app.listen(process.env.PORT || DEFAULT_PORT, function(){
-    console.log('awesome appointments app running on port 3000, go to http://localhost:3000') 
- }); 
+app.listen(process.env.PORT || DEFAULT_PORT, function () {
+    console.log('awesome appointments app running on port 3000, go to http://localhost:3000')
+}); 
