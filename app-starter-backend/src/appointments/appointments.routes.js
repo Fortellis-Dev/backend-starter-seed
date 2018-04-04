fe83da0bf37bc40289db3876fc292c9ce7d6cfd8
Naveen, Dokuparthi <naveen.dokuparthi@cdk.com>
2018-04-03 21:08:58 -0500

    Cleaning up un-used directory

--- app-starter-backend/src/appointments/appointments.routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const appointmentsrepo = require('./appointments.repo');
-const appointmentsController = require('./appointments.controller')(appointmentsrepo);
-
-module.exports = function (app) {
-    app.get('/appointments', appointmentsController.list);
-}
